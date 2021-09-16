7c53bbc4de7e66e629a4b24a348e3fc1d626daad
dimsp4 <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>
2021-09-16 11:20:04 +0700

    tugastiga

--- Tugas3/app/src/main/java/com/example/tugas_3/HomeActivity.kt
+++ Tugas3/app/src/main/java/com/example/tugas_3/HomeActivity.kt
@@ -1,12 +1,13 @@
 package com.example.tugas_3
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class HomeActivity : AppCompatActivity{
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -20,17 +21,18 @@ class HomeActivity : AppCompatActivity{
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.bahasa -> {
-                Toast.makeText(, this, "Pengaturan Bahasa", Toast.LENGTH_SHORT).show()
+                Toast.makeText( this, "Pengaturan Bahasa", Toast.LENGTH_SHORT).show()
                 return true
             }
             R.id.tampilan -> {
-                Toast.makeText(, this, "Pengaturan Tampilan", Toast.LENGTH_SHORT).show()
+                Toast.makeText( this, "Pengaturan Tampilan", Toast.LENGTH_SHORT).show()
                 return true
             }
             R.id.logout -> {
                 startActivity(Intent(this, LoginActivity::class.java))
                 return true
             }
+
         }
     }
 }
