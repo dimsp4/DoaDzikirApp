5c6e9197ed0cba4a87e4452d4055b4f3ea2fa205
dimsp4 <68719274+dimsp4@users.noreply.github.com>
2021-10-25 08:39:19 +0700

    Delete KotlinFundamental directory

--- KotlinFundamental/app/src/main/java/com/example/kotlinfundamental/kotlinfundamentalidn/oop/Visibility Modifier.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kotlinfundamental.kotlinfundamentalidn.oop
-
-fun main(){
-    val animal = Animal()
-    animal.info() //bisa jalan karna modifiernya 'public'
-    animal.umur // error karena
-}
